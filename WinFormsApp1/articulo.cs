377870f73c9d23d23721ab9ab107bdcc306c9737
Guido <sercoguido@gmail.com>
2024-09-04 00:21:06 -0300

    Voceto de Interfaz y Creacion clases

---
+++ WinFormsApp1/articulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public marca Marca { get; set; }
+        public categoria Categoria { get; set; }
+        public List<imagen> Imagenes { get; set; }
+        public decimal Precio { get; set; }
+
+        public articulo()
+        {
+            Imagenes = new List<imagen>();
+        }
+
+    }
+}
---
+++ WinFormsApp1/imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class imagen
+    {
+        public int Id { get; set; }
+        public string Ruta { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
